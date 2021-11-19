951519c8943b21341f4fd445c5e356d7ad5238d6
darothmedia <87622162+darothmedia@users.noreply.github.com>
2021-11-18 20:12:39 -0800

    users controller

--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -1,2 +1,36 @@
 class Api::UsersController < ApplicationController
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      login(@user)
+      render '/'
+    else
+      render json: @user.errors.full_messages, status: 401
+    end
+  end
+
+  def update
+
+  end
+
+  def show
+
+  end
+
+  def index
+
+  end
+
+  def destroy
+
+  end
+
+  private
+  def select_user
+    User.find(params[:id])
+  end
+
+  def user_params
+    params.require(:user).permit(:email, :fname, :lname, :password)
+  end
 end
