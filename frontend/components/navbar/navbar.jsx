d2a751d9ca374df95476bf038165f7c0e9157b83
darothmedia <87622162+darothmedia@users.noreply.github.com>
2021-11-21 17:00:41 -0800

    finish toggle

--- frontend/components/navbar/navbar.jsx
+++ frontend/components/navbar/navbar.jsx
@@ -2,17 +2,24 @@ import React from "react";
 import { Link } from 'react-router-dom'
 
 export default class Navbar extends React.Component {
+  constructor(props){
+    super(props)
+    
+  }
+
   render(){
     return(
-      <div id='navbar'>
+      <section id='navbar'>
         <div id='left-nav'>
           <Link to='/'>Explore</Link>
         </div>
+        <div id='center'>
         <div id='logo'>
           <Link to='/'>
             <img src='https://hikers-guide.s3.us-west-1.amazonaws.com/logo/img/logo/hg-solo-logo4x.png' alt='HikersGuide logo' />
           </Link>
-          <img src="https://hikers-guide.s3.us-west-1.amazonaws.com/logo/img/buttons/ttg-button-inactive%404x.png" alt="To The Galaxy" id='ttg'/>
+        </div>
+          <button id='ttg'>TO THE GALAXY</button>
         </div>
         <div id='right-nav'>
           <Link to='/signup'>
@@ -21,11 +28,8 @@ export default class Navbar extends React.Component {
           <Link to='/login'>
             <button id='log-in-button'>Log In</button>
           </Link>
-
         </div>
-
-        
-      </div>
+      </section>
     )
   }
 }
--- frontend/hikers-guide.jsx
+++ frontend/hikers-guide.jsx
@@ -2,25 +2,26 @@ import React from "react";
 import ReactDOM from "react-dom";
 import Root from './components/root';
 import configureStore from './store/store'
-import { createBrowserHistory } from 'history'
 
 document.addEventListener("DOMContentLoaded", () => {
   const root = document.getElementById('root');
   const store = configureStore()
-  const history = createBrowserHistory()
+
   ReactDOM.render(<Root store={store}/>, root)
+
+
   const ttg = document.getElementById('ttg')
-  const currentTheme = localStorage.getItem('theme')
-  if (currentTheme === 'galaxy') {
-    document.body.classList.add('galaxy-theme')
-  }
+  const navbar = document.getElementById('navbar')
+  const splash = document.getElementById('splash')
+
   ttg.addEventListener('click', () => {
     document.body.classList.toggle('galaxy');
+    navbar.classList.toggle('galaxy');
+    splash.classList.toggle('galaxy');
     let theme = 'light'
     if (document.body.classList.contains('galaxy')) {
       theme = 'galaxy'
     }
     localStorage.setItem('theme', theme)
-    console.log(localStorage.theme)
   })
 })
