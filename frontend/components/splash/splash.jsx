c4498740f7e312a5a90fc0af953ad56afb15c45e
darothmedia <87622162+darothmedia@users.noreply.github.com>
2021-11-22 17:07:51 -0800

    update splash

--- frontend/components/splash/splash.jsx
+++ frontend/components/splash/splash.jsx
@@ -4,7 +4,7 @@ import SearchBar from "./search_bar";
 export default class Splash extends React.Component {
   constructor(props){
     super(props)
-      this.tagline = this.props.tagline
+      this.tagline = ['trail', 'adventure', 'galaxy', 'inspiration', 'planet']
   }
 
   render(){
@@ -12,7 +12,7 @@ export default class Splash extends React.Component {
       <div id='splash'>
         <section id='search-spread'>
           <header>
-            <h1>Find your next {this.tagline}</h1>
+            <h1>Find your next {this.tagline[Math.floor(Math.random() * this.tagline.length)]}</h1>
           </header>
           <SearchBar />
         </section>
