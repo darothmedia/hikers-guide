f9276d8681c109a3f9cf8ad6365126425c4450c3
darothmedia <87622162+darothmedia@users.noreply.github.com>
2021-11-30 17:29:16 -0800

    update seedfile

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,6 +6,8 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
+require 'open-uri'
+
 User.destroy_all
 demo = User.create(fname: 'demo', lname: 'user', email: 'demo@user.com', password:'password')
 u1 = User.create(fname: 'Adrian', lname: 'Rothschild', email: 'adrian@hikersguide.com', password:'password')
@@ -68,6 +70,9 @@ t1 = Trail.create(
   park_id: p1.id,
   m_photo: purl('IMG_8338.jpg')
 )
+
+ph1 = open('https://hikers-guide-dev.s3.us-west-1.amazonaws.com/img/IMG_8338.jpg')
+t1.main_photo.attach(io: ph1, filename: 'IMG_8338.jpg')
 
 t2 = Trail.create(
   name: "Inspiration Point Trail",
